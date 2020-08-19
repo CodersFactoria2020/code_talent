e7db73bf8d67a9f1a89d7d4971fc44c6a24d6ebb
Diego <diegonskt@gmail.com>
2020-08-19 14:44:30 +0200

    refactoring codeAcademyScrapingTest

--- tests/Feature/CodeAcademyScrapingTest.php
+++ tests/Feature/CodeAcademyScrapingTest.php
@@ -10,47 +10,41 @@ use App\CodeAcademyScraping;
 class CodeAcademyScrapingTest extends TestCase
 {
     private $url = "https://www.codecademy.com/profiles/sergioliveresamor_fullstackphysio";
-    
-    /*
+    private $completedCourses;
+    private $scraper;
+
     public function setUp() :void
     {
-        
+        $this->scraper = new CodeAcademyScraping();
+        $this->completedCourses = $this->scraper->getCompletedCourses($this->url);
     }
-    */
+
     public function test_returns_an_array()
     {
-        $scraper = new CodeAcademyScraping();
-        $completedCourses = $scraper->getCompletedCourses($this->url);
-
-        $this->assertIsArray($completedCourses);
+        $this->assertIsArray($this->completedCourses);
     }
 
     public function test_returns_completed_courses()
     {
         $numberOfCompletedCourses = 4;
 
-        $scraper = new CodeAcademyScraping();
-        $completedCourses = $scraper->getCompletedCourses($this->url);
-
-        $this->assertEquals($numberOfCompletedCourses, count($completedCourses));
-        $this->assertContains('Learn HTML', $completedCourses);
+        $this->assertEquals($numberOfCompletedCourses, count($this->completedCourses));
+        $this->assertContains('Learn HTML', $this->completedCourses);
     }
 
 
     public function test_returns_last_connection()
     {
-        $scraper = new CodeAcademyScraping();
-        $lastConnection = $scraper->lastConnection($this->url);
+
+        $lastConnection = $this->scraper->lastConnection($this->url);
 
         $this->assertTrue(str_contains($lastConnection, 'Last coded'));
     }
 
     public function test_get_about_courses()
     {
-        $scraper = new CodeAcademyScraping();
 
-        $completedCourses = $scraper->getCompletedCourses($this->url);
-        $html_and_css_courses = $scraper->get_html_and_css_courses($completedCourses);
+        $html_and_css_courses = $this->scraper->get_html_and_css_courses($this->completedCourses);
 
         $this->assertContains('Learn HTML', $html_and_css_courses);
         $this->assertContains('Learn CSS', $html_and_css_courses);
@@ -60,11 +54,9 @@ class CodeAcademyScrapingTest extends TestCase
 
     public function test_get_json_data()
     {
-        $scraper = new CodeAcademyScraping();
 
-        $completedCourses = $scraper->getCompletedCourses($this->url);
-        $html_and_css_courses = $scraper->get_html_and_css_courses($completedCourses);
-        $scraper->get_json_data($html_and_css_courses);
+        $html_and_css_courses = $this->scraper->get_html_and_css_courses($this->completedCourses);
+        $this->scraper->get_json_data($html_and_css_courses);
 
         $json = file_get_contents('HTML_CSS_course.json');
         $json_data = json_decode($json, true);
