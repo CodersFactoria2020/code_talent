fee1ab7a7d5df61e3dc97d1412e8f2fa48827413
Sergi <solive@uic.es>
2020-08-19 13:48:59 +0200

    refactor SoloLearn unit test with mock

--- tests/Unit/SoloLearnScrapingTest.php
+++ tests/Unit/SoloLearnScrapingTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+
 use App\SoloLearnScraping;
 
 class SoloLearnScrapingTest extends TestCase
@@ -10,30 +11,42 @@ class SoloLearnScrapingTest extends TestCase
     private $scrappedCourses;
 
     public function test_get_specific_course()
-    {   
+    {     
         $scrapy = $this->partialMock(SoloLearnScraping::class, function ($mock) {
             $allCourses = include 'tests\Unit\AllCoursesMock.php';
             $mock->shouldReceive('getAllCourses')->andReturns($allCourses);
         });
-
+        
         $this->scrappedCourses = $scrapy->getAllCourses('');
+
         $this->assertContains('PHP Tutorial', $this->scrappedCourses[4]);
     }
-    /*
+    
     public function test_get_about_course( )
     {
-        $scrapy = new SoloLearnScraping();
+        $scrapy = $this->partialMock(SoloLearnScraping::class, function ($mock) {
+            $allCourses = include 'tests\Unit\AllCoursesMock.php';
+            $mock->shouldReceive('getAllCourses')->andReturns($allCourses);
+        });
+        
+        $this->scrappedCourses = $scrapy->getAllCourses('');
+       
         $php_course = $scrapy->get_PHP_course($this->scrappedCourses);
 
         $this->assertContains('PHP Tutorial',$php_course);
         $this->assertContains('100',$php_course);
         $this->assertContains('260 XP',$php_course);
     }
-
+    
 
     public function test_get_json_data()
     {
-        $scrapy = new SoloLearnScraping();
+        $scrapy = $this->partialMock(SoloLearnScraping::class, function ($mock) {
+            $allCourses = include 'tests\Unit\AllCoursesMock.php';
+            $mock->shouldReceive('getAllCourses')->andReturns($allCourses);
+        });
+        
+        $this->scrappedCourses = $scrapy->getAllCourses('');
         $php_course = $scrapy->get_PHP_course($this->scrappedCourses);
 
         $scrapy->get_json_data($php_course);
@@ -43,5 +56,5 @@ class SoloLearnScrapingTest extends TestCase
 
         $this->assertEquals($php_course, $json_data);
     }
-    */
+    
 }
