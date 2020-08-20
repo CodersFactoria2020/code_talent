4a9c25707693177c2ad9b7e5e942279c1faa8ea7
Javier <javiermarmolperez@gmail.com>
2020-08-20 12:38:08 +0200

    cambio vista promotion.show

---
+++ code_talent/app/Candidate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Candidate extends Model
+{
+    protected $fillable = ['name', 'lastname', 'email', 'promotion_id', 'phone_number', 'status', 'soloLearn', 'codeAcademy'];
+
+    public function promotion()
+    {
+        return $this->belongsTo(Promotion::class);
+    }
+
+}
