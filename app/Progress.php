95ff1ea568b8f12d80326c067f59adeca88c8649
Diego <diegonskt@gmail.com>
2020-08-24 09:16:18 +0200

    test if Sololearn have a course name

---
+++ app/Progress.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Progress extends Model
+{
+    public function getPhpCourseInfo($dataAllCourse)
+    {
+        return ['PHP Tutorial'];
+    }
+}
