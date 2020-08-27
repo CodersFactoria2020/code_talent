dab17ee9baf6d0debb28f8fd9f480d8a29a2d1e2
Sergi <solive@uic.es>
2020-08-27 18:20:38 +0200

    created CandidateObserver

---
+++ app/Observers/CandidateObserver.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Observers;
+
+use App\Candidate;
+
+class CandidateObserver
+{
+    public function created(Candidate $candidate)
+    {
+        //
+    }
+
+    public function updated(Candidate $candidate)
+    {
+        //
+    }
+
+    public function deleted(Candidate $candidate)
+    {
+        //
+    }
+
+    public function restored(Candidate $candidate)
+    {
+        //
+    }
+
+    public function forceDeleted(Candidate $candidate)
+    {
+        //
+    }
+}
