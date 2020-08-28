b2c6f5a0783953927fa19811b5a4c9d2a138f06f
Diego <diegonskt@gmail.com>
2020-08-28 11:10:29 +0200

    make candidateObserve, CourseSeeder, relationships Promotion Course

--- app/Observers/CandidateObserver.php
+++ app/Observers/CandidateObserver.php
@@ -3,12 +3,19 @@
 namespace App\Observers;
 
 use App\Candidate;
+use App\CodeAcademyScraping;
+use App\Progress;
 
 class CandidateObserver
 {
+
     public function created(Candidate $candidate)
     {
-        //
+        $scrappy_codeAcademy = new CodeAcademyScraping($candidate);
+        $course = $candidate->promotion->course;
+
+        //$progress_codeAcademy = Progress::fromCodeAcademy($scrappy_codeAcademy, $course);
+
     }
 
     public function updated(Candidate $candidate)
@@ -16,15 +23,18 @@ class CandidateObserver
         //
     }
 
+
     public function deleted(Candidate $candidate)
     {
         //
     }
 
+
     public function restored(Candidate $candidate)
     {
         //
     }
+
 
     public function forceDeleted(Candidate $candidate)
     {
