136c0531c9d3b559fb36976c7e60ba07e1606230
Sergi <solive@uic.es>
2020-08-31 13:19:37 +0200

    added course platform and creates progress from each platform

--- database/migrations/2020_05_20_101707_create_courses_table.php
+++ database/migrations/2020_05_20_101707_create_courses_table.php
@@ -12,6 +12,7 @@ class CreateCoursesTable extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('platform');
             $table->timestamps();
         });
     }
